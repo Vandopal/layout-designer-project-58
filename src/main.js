38e2dea45e578fca32006d333e896f3ebd1fad30
Vandopal <petya-pavlov-20001@mail.ru>
2025-09-14 14:31:33 +0500

    label --fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const label = document.querySelector('.label')
 
-document.querySelector('.label').addEventListener('focus', function() {
+label.addEventListener('focus', function() {
     if (this.value === 'Ваша электронная почта') {
         this.value = '';
     }
